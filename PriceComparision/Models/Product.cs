5a663f11d3428321df733c34ddc98e04a5e5ebd3
YoungNavigator <andriej2301@gmail.com>
2024-04-09 15:39:51 +0200

    feat: Database connection

---
+++ PriceComparision/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceComparision.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Shop { get; set; }
+    }
+}
